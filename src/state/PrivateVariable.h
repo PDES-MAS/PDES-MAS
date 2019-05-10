18edbc8fcdd5f33ccf650cc520153b8ea8136da7
CubicPill <cubicpill@gmail.com>
2019-05-10 23:28:32 +0800

    save

--- src/state/PrivateVariable.h
+++ src/state/PrivateVariable.h
@@ -5,11 +5,23 @@
 #ifndef PDES_MAS_PRIVATEVARIABLE_H
 #define PDES_MAS_PRIVATEVARIABLE_H
 
+#include <variant>
+#include <string>
+#include <types/Point.h>
+
 using namespace std;
 namespace pdesmas {
+  template<class T>
   class PrivateVariable {
+  private:
+    T value_;
+  public:
+    void set_value(T v);
 
+    T get_value();
   };
+
+
 }
 
 
